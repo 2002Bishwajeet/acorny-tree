eed351d2ac9a13e944b684a4754cb7b2747c5c58
Agon Noga <github@noga.dev>
2021-06-19 17:13:27 +0300

    help debug screens

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,14 @@
+import 'package:acorny_tree/screens/homepage.dart';
 import 'package:acorny_tree/screens/onboarding.dart';
+import 'package:acorny_tree/screens/quest_screen.dart';
+import 'package:acorny_tree/screens/shop_screen.dart';
 import 'package:acorny_tree/themes.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 // import 'package:flutter/services.dart';
 
 import 'package:flutter/scheduler.dart' show timeDilation;
+import 'package:flutter/services.dart';
 
 void main() {
   timeDilation = .5;
@@ -22,6 +27,37 @@ class AcornyTrees extends StatelessWidget {
   Widget build(BuildContext context) => MaterialApp(
         theme: Themes.generalTheme(context),
         debugShowCheckedModeBanner: false,
-        home: const OnboardingScreen(),
+        routes: {
+          '/home': (context) => const HomePage(),
+          '/onboard': (context) => const OnboardingScreen(),
+          '/shop': (context) => const ShopScreen(),
+          '/quest': (context) => const QuestScreen(),
+        },
+        home: kDebugMode ? const AppWrapper() : const OnboardingScreen(),
       );
+}
+
+class AppWrapper extends StatelessWidget {
+  const AppWrapper({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return RawKeyboardListener(
+      focusNode: FocusNode(),
+      onKey: (RawKeyEvent key) {
+        if (key.logicalKey == LogicalKeyboardKey.keyQ) {
+          Navigator.pushReplacementNamed(context, '/quest');
+        } else if (key.logicalKey == LogicalKeyboardKey.keyS) {
+          Navigator.pushReplacementNamed(context, '/shop');
+        } else if (key.logicalKey == LogicalKeyboardKey.keyH) {
+          Navigator.pushReplacementNamed(context, '/home');
+        } else if (key.logicalKey == LogicalKeyboardKey.keyO) {
+          Navigator.pushReplacementNamed(context, '/onboard');
+        }
+      },
+      child: const OnboardingScreen(),
+    );
+  }
 }
