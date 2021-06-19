fc2658679170f8a183094e56f2ce977c47cf586a
Agon Noga <github@noga.dev>
2021-06-19 17:56:38 +0300

    removed onboarding introduction package

--- lib/screens/onboarding.dart
+++ lib/screens/onboarding.dart
@@ -2,12 +2,10 @@ import 'package:acorny_tree/screens/quest_screen.dart';
 import 'package:acorny_tree/widgets/app_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:introduction_screen/introduction_screen.dart';
 import 'package:line_icons/line_icon.dart';
 import 'package:lottie/lottie.dart';
 
 import '../themes.dart';
-import 'shop_screen.dart';
 
 class OnboardingScreen extends StatefulWidget {
   const OnboardingScreen({Key? key}) : super(key: key);
@@ -18,11 +16,6 @@ class OnboardingScreen extends StatefulWidget {
 
 class _OnboardingScreenState extends State<OnboardingScreen> {
   final _moneyController = TextEditingController();
-
-  static const _pageDecor = PageDecoration(
-    bodyAlignment: Alignment.center,
-    titleTextStyle: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
-  );
 
   @override
   Widget build(BuildContext context) {
